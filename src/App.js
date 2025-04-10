bd02023fdfb2c369c3b9c842c659fbfae787fdfe
rellz26 <fareldelisa26@gmail.com>
2025-04-10 19:02:15 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', color: 'white', background: '#111' }}>
+      <h1>Halo! Ini aplikasi keuangan pribadi sederhana</h1>
+      <p>Silakan tambahkan fitur React di sini...</p>
+    </div>
+  );
+}
+
+export default App;
